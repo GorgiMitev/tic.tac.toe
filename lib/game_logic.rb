d09b82e7d1ac3718d01325e590056cd9f2a65e6d
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-10 12:57:31 +0300

    Validate user input

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,3 +1,24 @@
 module GameLogic
-  @turns = @player1_turn + @player2_turn
+  def count_turns
+    @turns = @player1_turn + @player2_turn
+  end
+  
+
+  def user_position(position)
+    if position.between?(1, 9).to_i && @turns.any? {|turn| turn == position.to_i}
+      true
+    else
+      false
+    end
+  end
+
+
+
+
+
+
+
+
 end
+
+
