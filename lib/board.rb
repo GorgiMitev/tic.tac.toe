a2a08aad2ac37cc12e68f8a0cc63b1be20db5543
Gorgi <gorgimitev@gmail.com>
2021-04-12 17:13:15 +0200

    making changes to the logic of our game

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,13 @@
-
 class Board
 
-  def initialize(player1, player2)
-    
-    @player1 = player1
-    @player2 = player2
-    @game_board
-  end
-
-<<<<<<< HEAD
-  def self.player_name(name)
-    name.to_s.length > 3 && name.split('').all?(/[a-zA-Z]/)
-  end
-
-=======
+    def initialize(player1, player2)
+      
+      @player1 = player1
+      @player2 = player2
+    end
   
->>>>>>> 02895b2d660169bbfd40aadac3765b10ee15a5eb
+    def self.player_name(name)
+      name.to_s.length > 3 && name.split('').all?(/[a-zA-Z]/)
+    end
+  
 end
--- lib/player.rb
+++ lib/player.rb
@@ -5,8 +5,5 @@ class Player
     @name = name
     @marker = marker
   end
-
-  def self.player_name(name)
-    name.length >= 3 && name.split('').all?(/[a-zA-Z]/)
-  end
 end
+s
