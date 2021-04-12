5d63e05cd679e06f2dad985645599d94a0a72b38
Gorgi <gorgimitev@gmail.com>
2021-04-12 17:08:08 +0200

    making changes to our game_logic module

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,14 @@
+
 class Board
-  attr_accessor :game_board
 
   def initialize(player1, player2)
-    @game_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
     @player1 = player1
     @player2 = player2
   end
 
   def self.player_name(name)
-    name.length >= 3 && name.split('').all?(/[a-zA-Z]/)
+    name.to_s.length > 3 && name.split('').all?(/[a-zA-Z]/)
   end
+
 end
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -3,9 +3,8 @@ module GameLogic
     @turns = @player1_turn + @player2_turn
   end
   
-
   def user_position(position)
-    if position.between?(1, 9).to_i && @turns.any? {|turn| turn == position.to_i}
+    if position.between?(1..9).to_a && @turns.any? {|turn| turn == position.to_i}
       true
     else
       false
@@ -13,24 +12,12 @@ module GameLogic
   end
 
   def winner
-    win_sequence.each do |sequence|
-    player1_wins if sequence - @player1_turn == []
-    @win = true if sequence - @player1_turn == []
-  
-    player2_wins if sequence - @player2_turn == []
-    @win = true if sequence - @player2_turn == []
+    @win_sequence.each do |win_opt|
+      player1_wins if win_opt - @player1_trn == []
+      @win = true if win_opt - @player1_trn == []
+      player2_wins if win_opt - @player2_trn == []
+      @win = true if win_opt - @player2_trn == []
     end
     @win
   end
-  end
-
-  
-
-
-
-
-
-
-
-
-
+end
