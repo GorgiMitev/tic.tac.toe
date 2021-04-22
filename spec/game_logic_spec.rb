5f9847d357a26a0b5efe379737259d7f774d07f0
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-22 15:15:07 +0300

    Test cal_turns methods

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,14 +1,19 @@
-require_relative "../lib/game_logic"
+require_relative "../lib/game_logic.rb"
+require_relative('../lib/game')
 require "rspec"
 
+
+
+
 describe "Gamelogic" do
+  subject(:game_logic) {Game.new }
   let(:@player2_trn) {[]}
   let(:@player1_trn) {[]}
+
   
   describe "cal_turns" do
     it "returns sum of arry" do
-
-      expect(cal_turns).to eql([])
+      expect(subject.cal_turns).to eql([])
     end
   end
 end
