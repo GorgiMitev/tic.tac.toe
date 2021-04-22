1a5237b03c4ad8186ee711b41452fb2de72155c8
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-04-22 14:40:29 +0300

    Test player_name method

---
+++ spec/game_logic_spec.rb
@@ -0,0 +1,14 @@
+require_relative "../lib/game_logic"
+require "rspec"
+
+describe "Gamelogic" do
+  let(:@player2_trn) {[]}
+  let(:@player1_trn) {[]}
+  
+  describe "cal_turns" do
+    it "returns sum of arry" do
+
+      expect(cal_turns).to eql([])
+    end
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,40 @@
+require_relative "../lib/player"
+require "rspec"
+
+describe "Player" do
+
+  subject(:p1){ Player.new('Leon','X')}
+  let(:p2){Player.new('De','O')}
+
+  
+  describe "self.player_name" do
+    it 'should raise error if no parameters' do
+        expect{Player.new}.to raise_error ArgumentError
+    end
+
+    it 'should accept two parameters' do
+      expect{subject}.not_to raise_error ArgumentError
+  end
+
+    it "returns true if length of name is longer than 3" do
+      expect( Player.player_name(p1.name)).to be true
+    end
+
+    it "returns false if length of name is shorter than 3" do
+      expect(Player.player_name(p2.name)).to be false
+    end 
+
+    it "should take set name to first parameter" do
+      expect(subject.name).to eq('Leon')
+      expect(p2.name).to eq('De')
+    end
+
+    it "should take set marker to second parameter" do
+      expect(subject.marker).to eq('X')
+      expect(p2.marker).to eq('O')
+    end
+  end
+
+
+end
+
